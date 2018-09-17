a55bfb51819575e4b117253acefdbca9a28e65d3
pc lixo <aluno@naverecife.local>
2018-09-17 15:57:44 -0300

    pwp health
    rascinho

---
+++ BomberPets V.2.1/Assets/Scripts/PowerupHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupHealth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+
+		if (coll.gameObject.CompareTag("P1") && P1Movement.healthP1 < 3)
+		{
+			Debug.Log("vida P1 = " + P1Movement.healthP1);
+			P1Movement.healthP1 = P1Movement.healthP1 + 1;
+			Destroy(gameObject);
+		}
+		else{	Destroy(gameObject);	}
+	}
+}
